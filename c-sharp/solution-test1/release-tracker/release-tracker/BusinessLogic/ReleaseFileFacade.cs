1f472c0fd05510aa6ea2217e017b52fef2ec8b58
ronaldespinozato <ronald.espinoza.to@gmail.com>
2022-05-01 20:37:15 -0400

    class for compare files

---
+++ c-sharp/solution-test1/release-tracker/release-tracker/BusinessLogic/ReleaseFileFacade.cs
@@ -0,0 +1,32 @@
+﻿using release_tracker.ExternalFeatureData;
+using release_tracker.LocalDataAccess;
+using release_tracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace release_tracker.BusinessLogic
+{
+    public class ReleaseFileFacade
+    {
+        private ReleaseLocalDataAccess releaseLocalDataAccess;
+        public ReleaseFileFacade()
+        {
+            releaseLocalDataAccess = new ReleaseLocalDataAccess();
+        }
+
+        public ReleaseFile DownloadSaveReleaseFile(ReleaseRepository releaseRepository)
+        {
+
+            DownloadReleaseFile downloader = new DownloadReleaseFile(releaseRepository);
+            List<ReleaseFile> releases = downloader.getFileRelease();
+            ReleaseFile releaseFile = releases.ElementAt(0);
+
+            releaseLocalDataAccess.SaveReleaseFile(releaseFile);
+
+            return releaseFile;
+        }
+    }
+}
